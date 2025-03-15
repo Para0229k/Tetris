f5e27e3c04e2bef5c78418a73c0b74d50602e24a
CUTMR <ULi0933017633@gmail.com>
2025-03-15 23:15:07 +0800

    1.1版本
    旋轉bug修正、debug message修正

--- Tetris_game_1.0/main.py
+++ Tetris_game_1.0/main.py
@@ -8,39 +8,36 @@ from tetromino.tetromino import BrickManager
 from game.game import Game
 
 def main():
-    # 初始化
     pygame.init()
-    # 標題
     pygame.display.set_caption("俄羅斯方塊遊戲")
 
     # 全螢幕模式
     # canvas = pygame.display.set_mode((canvas_width, canvas_height), pygame.DOUBLEBUF and pygame.FULLSCREEN )
+    
     # 視窗模式
     canvas = pygame.display.set_mode((canvas_width, canvas_height))
 
+    # 設定
     clock = pygame.time.Clock()
-
     font = pygame.font.SysFont("simsunnsimsun", 24)
-
     brick_manager = BrickManager()
-
     game = Game(brick_manager)
-
     display = Display(canvas, font)
 
-    # 把方塊放入陣列
+    # 把方塊放入陣列(20x10)
     bricks_list = []
     for y in range(20):
         row = []
         for x in range(10):
             row.append(Box(pygame, canvas, "brick_x_" + str(x) + "_y_" + str(y), [ 0, 0, 26, 26], color_gray_block))
         bricks_list.append(row)
-    # 把方塊放入陣列
+    
+    # 把方塊放入陣列(4x4)
     for y in range(4):
         for x in range(4):
             brick_manager.bricks_next_object[x][y] = Box(pygame, canvas, "brick_next_x_" + str(x) + "_y_" + str(y), [ 0, 0, 26, 26], color_gray_block)
 
-    # 背景區域
+    # 背景
     background = Box(pygame, canvas, "background", [278, 18, 282, 562], color_gray)
     background_bricks_next = Box(pygame, canvas, "background_bricks_next", [590, 50, 114, 114], color_gray)
 
@@ -67,42 +64,55 @@ def main():
                 # esc - 退出
                 if event.key == pygame.K_ESCAPE:
                     running = False
-                # d - 除錯訊息
+                # D - 除錯訊息
                 elif event.key == pygame.K_d:
                     game.debug_message = not game.debug_message                
                 # 上 - 旋轉
                 elif event.key == pygame.K_UP and game.game_mode == 0:
-                    # 在邊界不能轉
-                    if (game.container_x == 8):
+                    # 保存當前狀態
+                    original_state = game.brick_state
+                    original_x = game.container_x
+                    
+                    # 討論不同情形
+                    # 在邊界
+                    if (game.container_x == 8 and game.brick_id == 6):
                         continue
-                    # S、N、I的情形
-                    if (game.brick_id == 1 or game.brick_id == 2 or game.brick_id == 7):
-                        if (game.brick_id == 7):
-                            if (game.container_x < 0 or game.container_x == 7):
-                                continue
-                        
-                        game.brick_state = game.brick_state + 1
-                        if (game.brick_state > 1):
-                            game.brick_state = 0                    
-                        
-                        brick_manager.transformToBricks(game.brick_id, game.brick_state)
-                        # 碰到方塊
-                        if (not brick_manager.ifCopyToBricksArray(game.container_x, game.container_y, game.bricks_array)):
-                            game.brick_state = game.brick_state - 1
-                            if (game.brick_state < 0):
-                                game.brick_state = 1
-                    # J、L、T的情形                                
+                    # S、Z
+                    if game.brick_id == 1 or game.brick_id == 2:
+                        new_state = (game.brick_state + 1) % 2
+                    # I
+                    elif game.brick_id == 7:
+                        if game.container_x < 0 or game.container_x == 7:
+                            continue
+                        new_state = (game.brick_state + 1) % 2                         
+                    # J、L、T     
                     elif (game.brick_id == 3 or game.brick_id == 4 or game.brick_id == 5):
-                        game.brick_state = game.brick_state + 1
-                        if (game.brick_state > 3):
-                            game.brick_state = 0                    
-                        
+                        new_state = (game.brick_state + 1) % 4
+                    # O
+                    else:
+                        continue
+
+                    # 更新狀態
+                    game.brick_state = new_state
+                    brick_manager.transformToBricks(game.brick_id, game.brick_state)
+                    
+                    # wall kick
+                    kick_offsets = [0, -1, 1, -2, 2]
+                    rotation_success = False
+                    for offset_x in kick_offsets:
+                        test_x = original_x + offset_x
+
+                        if brick_manager.ifCopyToBricksArray(test_x, game.container_y, game.bricks_array):
+                            game.container_x = test_x
+                            rotation_success = True
+                            break
+                    
+                    # 不能旋轉就復原狀態
+                    if not rotation_success:
+                        game.container_x = original_x
+                        game.brick_state = original_state
                         brick_manager.transformToBricks(game.brick_id, game.brick_state)
-                        # 碰到方塊
-                        if (not brick_manager.ifCopyToBricksArray(game.container_x, game.container_y, game.bricks_array)):
-                            game.brick_state = game.brick_state - 1
-                            if (game.brick_state < 0):
-                                game.brick_state = 3
+
                 # 下 - 加速
                 elif event.key == pygame.K_DOWN and game.game_mode == 0:
                     game.brick_down_speed = brick_drop_rapidly
@@ -141,37 +151,31 @@ def main():
                 # 放開下 - 恢復正常速度
                 if event.key == pygame.K_DOWN:
                     game.brick_down_speed = brick_down_speed_max
-         
-        # 清除畫面
+        
         canvas.fill(color_block)
 
-        # 遊戲進行中
+        # gamemode = 0 (遊戲進行中)
         if (game.game_mode == 0):
             # 方塊下降
             if(time_now >= game.brick_down_speed):
                 game.container_y = game.container_y + 1; 
-                # 碰到方塊就產生新的
+                
+                # 碰到方塊就產生下一個
                 if (not brick_manager.ifCopyToBricksArray(game.container_x, game.container_y, game.bricks_array)):
                     game.brickNew()            
                 brick_manager.transformToBricks(game.brick_id, game.brick_state)
                 time_now = 0
-        # 消除方塊
+        # gamemode = 1 (消除)
         elif (game.game_mode == 1):
             game.clearBrick()
-            # 回到遊戲進行中
+            # 回到遊戲
             game.game_mode = 0
             brick_manager.transformToBricks(game.brick_id, game.brick_state)
 
-        # 更新遊戲顯示
+        # 更新顯示內容
         display.updateGameDisplay(game, brick_manager, background, background_bricks_next, bricks_list)
-        
-        # 顯示除錯訊息
         display.displayDebugInfo(game, brick_manager)
-
-        # 顯示遊戲訊息
         display.displayGameInfo(game, clock)
-
-        # 更新畫面
         pygame.display.update()
         clock.tick(60)
 
